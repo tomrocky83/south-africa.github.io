4b322d1f8014c0301ae0843969072d8099904917
tomrocky83 <50927191+tomrocky83@users.noreply.github.com>
2019-11-17 14:58:32 -0500

    website done

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('hi')
+
+$(document).ready(function() {
+
+    $('#bloe').on('click', citypic)
+
+    function citypic(){
+      
+    
+        $('.bloe').show();
+        
+      }
+
+      $('#cape').on('click', citypic1)
+
+      function citypic1(){
+        
+      
+          $('.cape').show();
+          
+        }
+
+
+
+        $('#pre').on('click', citypic2)
+
+        function citypic2(){
+          
+        
+            $('.pre').show();
+            
+          }
+
+
+    // $('#city').click(function(){
+    //     $('p').show();
+    //     // $('p').css('display', 'inline-block')
+        
+    // });
+
+
+
+
+
+});
